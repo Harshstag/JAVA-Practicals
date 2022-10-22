64023027bbcc3203243806d51dcddfe76910a374
Harsh Singh <62803431+Harshstag@users.noreply.github.com>
2022-10-22 14:18:42 +0530

    Add files via upload

---
+++ Practical_3/calculatePackage.java
@@ -0,0 +1,11 @@
+package calculatePackage;
+
+public class calculatePackage
+{
+
+    public void CircleArea(double r) {
+        double area;
+        area = ((3.14)*(r * r));
+        System.out.println("Area of Circle is  Radius "+ r +" is :"+ area );
+    }
+}
---
+++ Practical_3/practical_3aa.java
@@ -0,0 +1,12 @@
+package pac2;
+import calculatePackage.*;
+
+class practical_3aa{
+
+    public static void main(String[] args)
+    {
+        calculatePackage obj = new calculatePackage();
+        obj.CircleArea(3);
+    }
+    
+}
